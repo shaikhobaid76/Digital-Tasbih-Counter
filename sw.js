6fac5c2e6de6b9f7baf39ff0d07f8ca85f266adb
shaikhobaid76 <shaobaid76@gmail.com>
2026-03-04 04:06:24 +0530

    Added Tasbih counter project files

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'tasbih-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',  // agar aapka HTML file index.html hai
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@600&family=Amiri:wght@700&display=swap'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Activate event (clean old caches)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
